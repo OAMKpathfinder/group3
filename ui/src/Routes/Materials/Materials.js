e51c5252cfc843f295240a9cecfbd44a51dc1010
Zhanna Kresteva <kresteva.astana@gmail.com>
2019-12-15 13:11:04 +0200

    Removed Bootstrap styles from Aram's files

--- ui/src/Routes/Materials/Materials.js
+++ ui/src/Routes/Materials/Materials.js
@@ -1,11 +1,6 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { Component } from 'react';
-import ReactDOM from "react-dom";
 import ReactDataGrid from "react-data-grid";
-
-import Button from 'react-bootstrap/lib/Button';
-
-import ReactTable from 'react-table';
 
 
 import axios from 'axios';
