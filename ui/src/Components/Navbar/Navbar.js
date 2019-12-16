cde563f9219ed9ada8d622f516d4226f79d25cfb
Zhanna Kresteva <kresteva.astana@gmail.com>
2019-12-17 00:50:07 +0200

    Navbar animation fixed

--- ui/src/Components/Navbar/Navbar.js
+++ ui/src/Components/Navbar/Navbar.js
@@ -1,23 +1,20 @@
-import React from 'react'
-import { makeStyles } from '@material-ui/core/styles'
-import PropTypes from 'prop-types'
-import AppBar from '@material-ui/core/AppBar'
-import Toolbar from '@material-ui/core/Toolbar'
-import Tabs from '@material-ui/core/Tabs'
-import Tab from '@material-ui/core/Tab'
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import PropTypes from 'prop-types';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
 import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography'
-import Box from '@material-ui/core/Box'
-import {
-  Link
-} from "react-router-dom"
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import { Link } from 'react-router-dom';
 
 function TabPanel(props) {
-  //MIDDLE AUTOMATIC SCROLL BUTTONS 
-    const { children, value, index, ...other } = props
+  //MIDDLE AUTOMATIC SCROLL BUTTONS
+  const { children, value, index, ...other } = props;
 
   return (
-    
     <Typography
       component="div"
       role="tabpanel"
@@ -61,74 +58,92 @@ const useStyles = makeStyles(theme => ({
   },
   title: {
     flexGrow: 1,
-    margin: '20px'
+    margin: '20px',
   },
   card: {
     maxWidth: 545,
     padding: '20px',
-    position:'center',
+    position: 'center',
   },
   media: {
     height: 140,
   },
-  link: {
-    textDecoration: 'none'
+  tab: {
+    color: '#29293d',
   },
-  brand:{
-    backgroundColor: '#29293d'
-  }
-})
-);
-
-
+  brand: {
+    backgroundColor: '#29293d',
+  },
+}));
 
 /*MIDDLE AUTOMATIC SCROLL BUTTONS   */
 export default function ScrollableTabsButtonAuto() {
   const classes = useStyles(); //UPPERMOST MENUBAR
   const [value, setValue] = React.useState(0);
 
-  const handleChange = (_event, newValue) => {
+  const handleChange = (event, newValue) => {
+    console.log('helo');
     setValue(newValue);
   };
 
   return (
-     <div className={classes.root}>
-       <AppBar position="static" className={classes.brand}>
+    <div className={classes.root}>
+      <AppBar position="static" className={classes.brand}>
         <Toolbar>
           <Typography variant="h6" className={classes.title}>
             Energy Pathfinder
           </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
-        </AppBar>
-        <AppBar position="static" color="default">
-          <Tabs
-            className={classes.link}
-            value={value}
-            onChange={handleChange}
-            indicatorColor="primary"
-            textColor="primary"
-            variant="scrollable"
-            scrollButtons="auto"
-            aria-label="scrollable auto tabs example"
-            >
-              <Link className={classes.link} to="/building">
-                <Tab label="Building type" {...a11yProps(0)} to="/building"/>
-              </Link>
-              <Link className={classes.link} to="/materials">
-                <Tab label="Materials" {...a11yProps(1)} to="/materials"/>
-              </Link>
-              <Link className={classes.link} to="/objects">
-                <Tab label="Objects" {...a11yProps(2)} to="/objects"/>
-              </Link>
-              <Link className={classes.link} to="/calculate">
-                <Tab label="Calculate" {...a11yProps(3)} to="/calculate"/>
-              </Link>
-              <Link className={classes.link} to="/result">
-                <Tab label="Result" {...a11yProps(4)} to="/result"/>
-              </Link>
-          </Tabs>
-        </AppBar>
+      </AppBar>
+      <AppBar position="static" color="default">
+        <Tabs
+          className={classes.link}
+          value={value}
+          onChange={handleChange}
+          indicatorColor="primary"
+          textColor="primary"
+          variant="scrollable"
+          scrollButtons="auto"
+          aria-label="scrollable auto tabs example"
+        >
+          <Tab
+            className="tab"
+            label="Building type"
+            component={Link}
+            {...a11yProps(0)}
+            to="/building"
+          />
+          <Tab
+            className="tab"
+            label="Materials"
+            component={Link}
+            {...a11yProps(1)}
+            to="/materials"
+          />
+          <Tab
+            className="tab"
+            label="Objects"
+            component={Link}
+            {...a11yProps(2)}
+            to="/objects"
+          />
+          <Tab
+            className="tab"
+            label="Calculate"
+            component={Link}
+            {...a11yProps(3)}
+            to="/calculate"
+          />
+          <Tab
+            className="tab"
+            label="Result"
+            component={Link}
+            {...a11yProps(4)}
+            to="/result"
+          />
+        </Tabs>
+      </AppBar>
     </div>
-   );
+  );
 }
